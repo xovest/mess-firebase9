f10a3fcce3db210f336fbe56dbf15e9b02557af7
xovest <79008917+xovest@users.noreply.github.com>
2021-12-15 18:26:33 +0200

    auth modif

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,17 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
 
-export const auth = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBW1PUWQtZhZmEx8DWGVJjAgwQhi3K1_Tw",
   authDomain: "auth-demo-c1925.firebaseapp.com",
   projectId: "auth-demo-c1925",
   storageBucket: "auth-demo-c1925.appspot.com",
   messagingSenderId: "746557330045",
   appId: "1:746557330045:web:059113e6cc70c0bb8d1714"
-}).auth()
+}
+
+initializeApp(firebaseConfig)
+
+const auth = getAuth()
+
+module.exports = auth;
